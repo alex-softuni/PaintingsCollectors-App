aea7b329586c2d39294b90e371c0e5f1a0dad24e
alex-softuni <notorious.atanasow@gmail.com>
2026-02-07 15:21:56 +0200

    feat: user registration implemented

--- src/main/java/com/paintingscollectors/user/service/UserService.java
+++ src/main/java/com/paintingscollectors/user/service/UserService.java
@@ -1,7 +1,41 @@
 package com.paintingscollectors.user.service;
 
+import com.paintingscollectors.user.model.User;
+import com.paintingscollectors.user.repository.UserRepository;
+import com.paintingscollectors.web.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
+
+    public void registerUser(@Valid RegisterRequest registerRequest) {
+        Optional<User> optionalUser = userRepository.findByUsername(registerRequest.getUsername());
+        if (optionalUser.isPresent()) {
+            throw new RuntimeException("User is already registered");
+        }
+
+        String encodedPassword = passwordEncoder.encode(registerRequest.getPassword());
+
+        User user = User.builder()
+                .username(registerRequest.getUsername())
+                .password(encodedPassword)
+                .email(registerRequest.getEmail())
+                .build();
+
+        userRepository.save(user);
+    }
 }
--- src/main/java/com/paintingscollectors/web/IndexController.java
+++ src/main/java/com/paintingscollectors/web/IndexController.java
@@ -1,14 +1,52 @@
 package com.paintingscollectors.web;
 
+import com.paintingscollectors.user.service.UserService;
+import com.paintingscollectors.web.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
+    private final UserService userService;
+
+    @Autowired
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
     public String getIndexPage() {
 
-        return "home";
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public ModelAndView getRegisterPage() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/register");
+        mav.addObject("registerRequest", new RegisterRequest());
+
+        return mav;
+    }
+
+    @PostMapping("/register")
+    public ModelAndView registerUser(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("/register");
+        }
+
+        userService.registerUser(registerRequest);
+
+        return new ModelAndView("redirect:/login");
+    }
+
+    @GetMapping("/login")
+    public ModelAndView getLoginPage() {
+        return new ModelAndView("login");
     }
 }
